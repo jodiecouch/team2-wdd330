436cad847bb053913fe69301bfe7e43a35b26fb7
jodiecouch <couchjodie@gmail.com>
2023-01-14 15:16:36 -0700

    fixed broken cart.

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,18 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cart = getLocalStorage("so-cart");
+  if (!Array.isArray(cart)) {
+    cart = [];
+  }
+
+  cart.push(product);
+  console.log(cart);
+
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
