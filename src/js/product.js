c3e1d40172afca9085e815afdb3a08d8fa152931
jodiecouch <couchjodie@gmail.com>
2023-01-28 10:48:36 -0700

    error fixed in index.html for script line to main.js

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,7 @@ import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
 
-const productId = getParam('product');
+const productId = getParam("product");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
@@ -32,5 +32,3 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
-
-
