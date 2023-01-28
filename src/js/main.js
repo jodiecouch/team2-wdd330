3751d0a9267fb6b108a37038659ef9a5c5aed192
jodiecouch <couchjodie@gmail.com>
2023-01-28 10:48:36 -0700

    error fixed in index.html for script line to main.js

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@ import ProductListing from "./ProductList.mjs";
 
 const tents = new ProductData("tents");
 const list = document.querySelector(".product-list");
-const productList = new ProductListing('Tents', tents, list);
+const productList = new ProductListing("Tents", tents, list);
 productList.init();
 
 console.log(productList);
